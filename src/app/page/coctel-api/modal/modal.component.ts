a0cc2b6b69f740a6c6c4a6880a0e22ac5eea32fa
jcoime8 <jcoime8@gmail.com>
2024-10-21 22:41:18 -0500

    Subiendo pagina completa

--- src/app/page/coctel-api/modal/modal.component.ts
+++ src/app/page/coctel-api/modal/modal.component.ts
@@ -1,11 +1,11 @@
 import { Component, ElementRef, Inject, Input, PLATFORM_ID, ViewChild } from '@angular/core';
 import { cocteles } from '../interfaces/coctel';
-import { isPlatformBrowser, NgIf } from '@angular/common';
+import { isPlatformBrowser, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgFor],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
@@ -16,29 +16,50 @@ export class ModalComponent {
   @ViewChild('modalElement') public modal!: ElementRef;
   private bootstrapModal: any;
   constructor(
-    @Inject(PLATFORM_ID) private plataformID:object
-  ){}
+    @Inject(PLATFORM_ID) private plataformID: object
+  ) { }
 
-  ngAfterViewInit(){
-    if(isPlatformBrowser(this.plataformID)){
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.plataformID)) {
       this.inicializarModal()
     }
   }
 
-  inicializarModal(){
-    import('bootstrap').then(bootstrap =>{
+  getIngredients(coctel: any): string[] {
+    return [
+      coctel['strIngredient1'],
+      coctel['strIngredient2'],
+      coctel['strIngredient3'],
+      coctel['strIngredient4'],
+      coctel['strIngredient5'],
+      coctel['strIngredient6'],
+      coctel['strIngredient7'],
+      coctel['strIngredient8'],
+      coctel['strIngredient9'],
+      coctel['strIngredient10'],
+      coctel['strIngredient11'],
+      coctel['strIngredient12'],
+      coctel['strIngredient13'],
+      coctel['strIngredient14'],
+      coctel['strIngredient15'],
+    ].filter(ingredient => ingredient); // Filtrar ingredientes vacíos
+  }
+
+
+  inicializarModal() {
+    import('bootstrap').then(bootstrap => {
       this.bootstrapModal = new bootstrap.Modal(this.modal.nativeElement)
     })
   }
 
-  open(coc: Partial<cocteles['drinks'][number]>){
+  open(coc: Partial<cocteles['drinks'][number]>) {
     this.coctel = coc
-    if(isPlatformBrowser(this.plataformID)){
-      if(this.bootstrapModal){
+    if (isPlatformBrowser(this.plataformID)) {
+      if (this.bootstrapModal) {
         this.bootstrapModal.show()
-      }else{
+      } else {
         this.inicializarModal()
-        setTimeout(()=>{
+        setTimeout(() => {
           this.bootstrapModal.show();
         }, 0)
       }
